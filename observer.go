a7d1afc846cbf803a23269f1fffd7d96ee5ea920
Sergey Skvokin <skvorkins@gmail.com>
2024-02-06 16:55:56 +0600

    fixed typo

--- observer.go
+++ observer.go
@@ -13,25 +13,25 @@ type Client struct {
 }
 
 type Deal struct {
-	ID                  int     `json:"id"`
-	CreatedAt           string  `json:"createdAt"`
-	FinishedAt          string  `json:"finishedAt"`
-	FinishExpupectingAt string  `json:"finishExpectingAt"`
-	DepartmentName      string  `json:"departmentName"`
-	Client              Client  `json:"client"`
-	Type                string  `json:"type"`
-	Status              string  `json:"status"`
-	Sum                 float64 `json:"sum"`
-	PaidSum             float64 `json:"paidSum"`
-	ReceiptPrinted      bool    `json:"receiptPrinted"`
-	CreatedById         int     `json:"createdById"`
-	Redo                bool    `json:"redo"`
-	ReadyStatusInStore  bool    `json:"readyStatusInStore"`
-	Company             string  `json:"dealCompany"`
-	CreatedUserName     string  `json:"createdUser_name"`
-	NPSStatus           string  `json:"npsStatus"`
-	RISStatus           string  `json:"risStatus"`
-	DBStatus            string  `json:"dbStatus"`
+	ID                 int     `json:"id"`
+	CreatedAt          string  `json:"createdAt"`
+	FinishedAt         string  `json:"finishedAt"`
+	FinishExpectingAt  string  `json:"finishExpectingAt"`
+	DepartmentName     string  `json:"departmentName"`
+	Client             Client  `json:"client"`
+	Type               string  `json:"type"`
+	Status             string  `json:"status"`
+	Sum                float64 `json:"sum"`
+	PaidSum            float64 `json:"paidSum"`
+	ReceiptPrinted     bool    `json:"receiptPrinted"`
+	CreatedById        int     `json:"createdById"`
+	Redo               bool    `json:"redo"`
+	ReadyStatusInStore bool    `json:"readyStatusInStore"`
+	Company            string  `json:"dealCompany"`
+	CreatedUserName    string  `json:"createdUser_name"`
+	NPSStatus          string  `json:"npsStatus"`
+	RISStatus          string  `json:"risStatus"`
+	DBStatus           string  `json:"dbStatus"`
 
 	Error error
 }
